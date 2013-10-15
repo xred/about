526c632f5f0942251145d41c37f34d6d37d58536
red xiao <hongxiao@hustunique.com>
2013-10-15 14:08:26 +0800

    version1 add title

--- home.js
+++ home.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 	$("#bg").ready(function(){
 		$("#bg").fadeIn(function(){
-			$("#wrapper").animate({marginTop:"-650px",opacity:"1"})
+			$("#aboutBox").animate({marginTop:"300px",opacity:"1"})
 		});
 	})
 })
