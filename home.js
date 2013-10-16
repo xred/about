0052026749f5243be797c37bbb9cd6f3f7410d72
red xiao <hongxiao@hustunique.com>
2013-10-16 10:00:06 +0800

    fixbug

--- home.js
+++ home.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 	$("#bg").ready(function(){
 		$("#bg").fadeIn(function(){
-			$("#aboutBox").animate({marginTop:"300px",opacity:"1"})
+			$("#aboutBox").animate({marginTop:"20%",opacity:"1"})
 		});
 	})
 })
